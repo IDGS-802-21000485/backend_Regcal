dc466898ea65946573e833b28ebb164a4daf6dbc
IDGS-802-21000485 <21000485@alumnos.utleon.edu.mx>
2025-06-22 18:43:12 -0600

    Correccion en el login

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,25 +1,16 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
+const usuarioSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   fechaNacimiento: { type: Date, required: true },
-  estatura: { type: Number, required: true, min: 100, max: 250 }, // en cm
-  peso: { type: Number, required: true, min: 30, max: 300 }, // en kg
-  actividad: { 
-    type: String, 
-    required: true,
-    enum: ['sedentario', 'ligero', 'moderado', 'intenso']
-  },
-  objetivo: {
-    type: String,
-    required: true,
-    enum: ['ligero', 'pesado']
-  },
-  caloriasMantenimiento: { type: Number },
-  caloriasObjetivo: { type: Number },
-  createdAt: { type: Date, default: Date.now }
+  estatura: { type: Number, required: true }, // en cm
+  peso: { type: Number, required: true }, // en kg
+  actividad: { type: String, enum: ['sedentario', 'ligero', 'moderado', 'activo', 'muy_activo'], required: true },
+  objetivo: { type: String, enum: ['ligero', 'pesado'], required: true },
+  caloriasMantenimiento: { type: Number, required: true },
+  caloriasObjetivo: { type: Number, required: true }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('Usuario', usuarioSchema);
